cb7379457f6cf7ded763c380efb22b7f4edb48d1
Vladimir-Bumagin <vl.bumagin@yandex.ru>
2023-03-11 22:49:19 +0300

    Исправляет замечания после первой проверки

--- source/js/index.js
+++ source/js/index.js
@@ -1,38 +1,19 @@
-const mainNavToggle = document.querySelectorAll('.main-nav__toggle');
-const menuOpenButton = document.querySelector('.main-nav__toggle--open');
-const menuCloseButton = document.querySelector('.main-nav__toggle--close');
+const mainNavToggle = document.querySelector('.main-nav__toggle');
+const mainHeader = document.querySelector('.header-main');
 const popUpMenu = document.querySelector('.pop-up-menu');
-const mapLink = document.querySelector('.map__link');
-const mapFrame = document.querySelector('.map__frame');
-const sliderWrapper = document.querySelectorAll('.slider__img-wrapper');
 
-if (popUpMenu.classList.contains('pop-up-menu--no-js')) {
-  popUpMenu.classList.remove('pop-up-menu--no-js')
-}
+mainHeader.classList.remove('header-main--no-js')
 
-mainNavToggle.forEach(element => {
-if (element.classList.contains('main-nav__toggle--no-js')) {
-  element.classList.remove('main-nav__toggle--no-js')
-}
-})
+popUpMenu.classList.remove('pop-up-menu--no-js')
 
-const openPopUp = function() {
-  menuOpenButton.addEventListener('click', (evt) => {
+mainNavToggle.classList.remove('main-nav__toggle--no-js')
+
+const togglePopUp = function() {
+  mainNavToggle.addEventListener('click', (evt) => {
   evt.preventDefault();
   popUpMenu.classList.toggle('pop-up-menu--is-open');
-  menuOpenButton.classList.toggle('visually-hidden');
-  menuCloseButton.classList.toggle('visually-hidden');
+  mainNavToggle.classList.toggle('main-nav__toggle--close');
   });
 }
 
-const closePopUp = function () {
-  menuCloseButton.addEventListener('click', (evt) => {
-    evt.preventDefault();
-    popUpMenu.classList.toggle('pop-up-menu--is-open');
-    menuOpenButton.classList.toggle('visually-hidden');
-    menuCloseButton.classList.toggle('visually-hidden');
-    });
-}
-
-openPopUp();
-closePopUp();
+togglePopUp();
